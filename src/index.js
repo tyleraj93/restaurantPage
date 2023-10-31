57d54adec0a9afc195382943e13efe24ff103fa5
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-10-31 19:22:16 -0400

    Add import of header function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const firstName = "Tyler";
-console.log(`Hello, World. It's ${firstName}!`);
+import buildHeaderBefore from "./header";
 
+const content = document.getElementById("content");
+
+//Build header
+buildHeaderBefore(content);
