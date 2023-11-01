a57201f21cc5b4e8545956897a94476cabc84ce0
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-11-01 18:02:21 -0400

    Complete functionality for a footer

---
+++ src/footer.js
@@ -0,0 +1,11 @@
+export default function buildFooter() {
+    const body = document.body;
+    const footerHolder = document.createElement("div");
+    const corp = document.createElement("p");
+    corp.textContent = "Odin's Hospitality Group";
+    footerHolder.appendChild(corp);
+    const company = document.createElement("p");
+    company.textContent = "Loki's Lair";
+    footerHolder.appendChild(company);
+    body.appendChild(footerHolder);
+}
