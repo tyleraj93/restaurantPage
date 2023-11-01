44b10e2287f38b538d720681e0b7da7292ed519f
Tyler J <57014233+tyleraj93@users.noreply.github.com>
2023-11-01 14:27:01 -0400

    Add import of home buildHome function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import buildHeaderBefore from "./header";
+import buildHome from "./home";
 
 const content = document.getElementById("content");
 
 //Build header
 buildHeaderBefore(content);
+buildHome(content);
